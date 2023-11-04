c5b05b5734c5e7ac09789c6b2bbf6821308f4c1b
TheRealFlarf <flarf@DESKTOP-D0MEQQ6.fios-router.home>
2023-11-04 13:22:24 -0400

    Added zinc/aluminum place/step sounds

--- src/main/java/net/mcreator/intech/block/AluminumTilesBlock.java
+++ src/main/java/net/mcreator/intech/block/AluminumTilesBlock.java
@@ -1,15 +1,18 @@
 
 package net.mcreator.intech.block;
 
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.common.util.ForgeSoundType;
+
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
 import java.util.List;
@@ -17,7 +20,11 @@ import java.util.Collections;
 
 public class AluminumTilesBlock extends Block {
 	public AluminumTilesBlock() {
-		super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(3f, 50f).requiresCorrectToolForDrops());
+		super(BlockBehaviour.Properties.of()
+				.sound(new ForgeSoundType(1.0f, 1.0f, () -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("intech:block.aluminum.break")), () -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("intech:block.aluminum.step")),
+						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("intech:block.aluminum.place")), () -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("intech:block.aluminum.hit")),
+						() -> ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("intech:block.aluminum.step"))))
+				.strength(3f, 50f).requiresCorrectToolForDrops());
 	}
 
 	@Override
